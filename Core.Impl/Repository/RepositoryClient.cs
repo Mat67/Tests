6c8fe9c621066bd3aa2627a0c58f8d6134ec160c
Mat67 <hemmerle.mathieu@outlook.com>
2016-08-05 19:46:46 +0200

    Initialisation des repositories et DTO

---
+++ Core.Impl/Repository/RepositoryClient.cs
@@ -0,0 +1,23 @@
+﻿using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DTO;
+
+namespace Core.Impl.Repository
+{
+    public class RepositoryClient : IRepositoryClient
+    {
+        public List<Client> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core.Impl/Repository/RepositoryProfil.cs
@@ -0,0 +1,23 @@
+﻿using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DTO;
+
+namespace Core.Impl.Repository
+{
+    public class RepositoryProfil : IRepositoryProfil
+    {
+        public Profil GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Profil GetByLibelle(string libelle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
