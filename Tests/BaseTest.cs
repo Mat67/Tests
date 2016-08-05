6b55d3c32f78bed628effa048563c54425fcfeeb
Mat67 <hemmerle.mathieu@outlook.com>
2016-08-05 20:41:56 +0200

    Organisation des tests

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [SetUpFixture]
+    public class BaseTest
+    {
+        public static Castle.Windsor.WindsorContainer Container = new Castle.Windsor.WindsorContainer();
+
+        [OneTimeSetUp]
+        public void BaseSetUp()
+        {
+            // Traitement effectué une seule fois au lancement des tests
+            // Initialisation base
+            // Chargement de données
+        }
+
+        [OneTimeTearDown]
+        public void BaseTearDown()
+        {
+            // Traitement effectué une seule fois après l'exécution des tests
+            // Suppression de la base
+        }
+    }
+}
---
+++ Tests/TestClient.cs
@@ -0,0 +1,38 @@
+﻿using Core.Repository;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestClient
+    {
+        IRepositoryClient _ClientRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _ClientRepository = BaseTest.Container.Resolve<IRepositoryClient>();
+        }
+
+        [Test]
+        public void TestGetById()
+        {
+            Assert.IsNotNull(_ClientRepository.GetById("1"));
+        }
+
+
+        [Test]
+        public void TestGetAll()
+        {
+            var resultat = _ClientRepository.GetAll();
+
+            Assert.IsNotNull(resultat);
+            Assert.Greater(resultat.Count, 0);
+        }
+    }
+}
